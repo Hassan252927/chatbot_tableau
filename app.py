2057c6d8c1c3e504479a98777afebbb2e32cd1f6
Muhammad Hassan Uozair <149463639+Hassan252927@users.noreply.github.com>
2025-03-17 21:51:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,18 +16,6 @@ ALLOWED_ORIGINS = ["https://chatbot-tableau.vercel.app/", "http://127.0.0.1:5500
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ALLOWED_ORIGINS}}, supports_credentials=True)  # ✅ Fixed CORS setup
 
-@app.after_request
-def add_cors_headers(response):
-    origin = request.headers.get("Origin")
-    if origin in ALLOWED_ORIGINS:
-        response.headers["Access-Control-Allow-Origin"] = origin
-        response.headers["Access-Control-Allow-Credentials"] = "true"
-    # else: don't set Access-Control-Allow-Origin at all for disallowed origins
-
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-    return response
-
 
 app.secret_key = "d39c892a9ef547d2917a12c3e3e1bd078f7ef3a9ffb2edb6dd65a12cf8f2f61a"
 
